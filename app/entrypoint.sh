6eedd5d69379c903de7c4c0bdddc9ed5866bcb20
szabos <steve.g.szabo@gmail.com>
2022-01-08 17:49:31 +0000

    updates

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,7 +2,13 @@
 
 PROFILE=/vault/secrets/config
 
-[ -f $PROFILE ] && source $PROFILE
+if [ -f $PROFILE ]; then
+    source $PROFILE
+else
+    POSTGRES_USER=user
+    POSTGRES_PASSWORD=pass
+    export POSTGRES_USER POSTGRES_PASSWORD
+fi
 
 /usr/bin/supervisord --nodaemon -c /usr/supervisord.conf
 
