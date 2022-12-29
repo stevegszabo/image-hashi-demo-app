cdab68b044e47a47689f17cab1232da84ef367c2
Steve Szabo <steve.g.szabo@gmail.com>
2022-12-28 20:21:18 -0500

    updates

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -10,6 +10,7 @@ else
     export POSTGRES_USER POSTGRES_PASSWORD
 fi
 
-/usr/bin/supervisord --nodaemon -c /usr/supervisord.conf
+###/usr/bin/supervisord --nodaemon -c /usr/supervisord.conf
+gunicorn --worker-class eventlet -w 1 main:app -b 0.0.0.0:5000
 
 exit $?
